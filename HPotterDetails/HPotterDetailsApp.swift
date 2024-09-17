c17250f58d71f2396921ad1c5124a9dcfadf7f50
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-09-17 23:12:33 +0900

    foundation with resources added

---
+++ HPotterDetails/HPotterDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPotterDetailsApp.swift
+//  HPotterDetails
+//
+//  Created by joe on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HPotterDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
