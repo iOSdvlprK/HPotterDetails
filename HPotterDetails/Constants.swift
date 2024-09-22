d658c33b91b5b3e870fae3b0a93d997a268f79f8
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-09-22 23:58:07 +0900

    build the instructions screen
    - view extension
    - button extension

--- HPotterDetails/Constants.swift
+++ HPotterDetails/Constants.swift
@@ -5,8 +5,28 @@
 //  Created by joe on 9/19/24.
 //
 
-import Foundation
+import SwiftUI
 
 enum Constants {
     static let hpFont = "PartyLetPlain"
+}
+
+struct InfoBackgroundImage: View {
+    var body: some View {
+        Image("parchment")
+            .resizable()
+            .ignoresSafeArea()
+            .background(.brown)
+    }
+}
+
+extension Button {
+    func doneButton() -> some View {
+        self
+            .font(.largeTitle)
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(.brown)
+            .foregroundStyle(.white)
+    }
 }
