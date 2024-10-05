f480456fe6fefdd55c618c8b5214cd5995496841
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-10-05 23:53:39 +0900

    in-app purchases: test purchases

--- HPotterDetails/HPotterDetailsApp.swift
+++ HPotterDetails/HPotterDetailsApp.swift
@@ -15,6 +15,9 @@ struct HPotterDetailsApp: App {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .task {
+                    await store.loadProducts()
+                }
         }
     }
 }
